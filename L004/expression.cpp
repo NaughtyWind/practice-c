5f13f02ee2a61343ed11fd095df7be44a1010f7d
NaughtyWind <yunxiaomxd@163.com>
2020-03-08 22:40:26 +0800

    添加条件表达式

---
+++ L004/expression.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // char ch;
+    // string str;
+    // cout << "input your name: " << endl;
+    // cin.get(ch);
+    // while (cin.fail() == false) {
+    //     str += ch;
+    //     cin.get(ch);
+    // }
+
+    // const int index = str.find("lxy");
+    // if (index > -1) {
+    //     cout << "my name is " << str.substr(index) << endl;
+    // } else {
+    //     cout << "name is not found" << endl;
+    // }
+
+    // char ch1;
+    // cout << "input number for you want to get MB:" << endl;
+    // cin >> ch1;
+    // cout << "MB size is" << ()
+
+    return 0;
+}
